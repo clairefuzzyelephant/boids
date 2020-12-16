a4e5e26dccb81f34f53a71e808ca0db1e2be3d12
Claire Cheng <clairecheng88@gmail.com>
2020-12-16 11:13:37 -0800

    added build folder

---
+++ build/CMakeFiles/boids.dir/DependInfo.cmake
@@ -0,0 +1,82 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/external/src/glad/src/glad.c" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/external/src/glad/src/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../external/src/glad/include"
+  "../external/src/imgui"
+  "../external/src/imgui/examples"
+  "../external/src/stb"
+  "../"
+  "../assignment_code/boids"
+  "../assignment_code/common"
+  "../external/src/glfw-3.3.2/include"
+  "../external/src/glm-0.9.9.8"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/assignment_code/boids/BoidApp.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/assignment_code/boids/BoidApp.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/assignment_code/boids/BoidNode.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/assignment_code/boids/BoidNode.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/assignment_code/boids/Flock.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/assignment_code/boids/Flock.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/assignment_code/boids/main.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/assignment_code/boids/main.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/assignment_code/common/helpers.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/assignment_code/common/helpers.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/external/src/imgui/examples/imgui_impl_glfw.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/external/src/imgui/examples/imgui_impl_glfw.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/external/src/imgui/examples/imgui_impl_opengl3.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/external/src/imgui/examples/imgui_impl_opengl3.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/external/src/imgui/imgui.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/external/src/imgui/imgui.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/external/src/imgui/imgui_demo.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/external/src/imgui/imgui_demo.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/external/src/imgui/imgui_draw.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/external/src/imgui/imgui_draw.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/external/src/imgui/imgui_widgets.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/external/src/imgui/imgui_widgets.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/gloo/Application.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/gloo/Application.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/gloo/Image.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/gloo/Image.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/gloo/InputManager.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/gloo/InputManager.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/gloo/MeshLoader.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/gloo/MeshLoader.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/gloo/Renderer.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/gloo/Renderer.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/gloo/Scene.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/gloo/Scene.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/gloo/SceneNode.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/gloo/SceneNode.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/gloo/Transform.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/gloo/Transform.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/gloo/VertexObject.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/gloo/VertexObject.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/gloo/cameras/ArcBallCameraNode.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/gloo/cameras/ArcBallCameraNode.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/gloo/cameras/BasicCameraNode.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/gloo/cameras/BasicCameraNode.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/gloo/components/CameraComponent.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/gloo/components/CameraComponent.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/gloo/components/RenderingComponent.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/gloo/components/RenderingComponent.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/gloo/components/ShadingComponent.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/gloo/components/ShadingComponent.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/gloo/debug/AxisNode.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/gloo/debug/AxisNode.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/gloo/debug/PrimitiveFactory.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/gloo/debug/PrimitiveFactory.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/gloo/gl_wrapper/BindGuard.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/gloo/gl_wrapper/BindGuard.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/gloo/gl_wrapper/BindableBuffer.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/gloo/gl_wrapper/BindableBuffer.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/gloo/gl_wrapper/VertexArray.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/gloo/gl_wrapper/VertexArray.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/gloo/parsers/ObjParser.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/gloo/parsers/ObjParser.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/gloo/shaders/PhongShader.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/gloo/shaders/PhongShader.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/gloo/shaders/ShaderProgram.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/gloo/shaders/ShaderProgram.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/gloo/shaders/SimpleShader.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/gloo/shaders/SimpleShader.cpp.o"
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/gloo/utils.cpp" "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/CMakeFiles/boids.dir/gloo/utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../external/src/glad/include"
+  "../external/src/imgui"
+  "../external/src/imgui/examples"
+  "../external/src/stb"
+  "../"
+  "../assignment_code/boids"
+  "../assignment_code/common"
+  "../external/src/glfw-3.3.2/include"
+  "../external/src/glm-0.9.9.8"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/fuzzyelephant/Documents/MIT/2020 Fall/6.837/boids/build/external/src/glfw-3.3.2/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
